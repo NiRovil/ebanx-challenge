086572c2dce1ec17459b7518e1f7f55507526916
NiRovil <nicolas199851@hotmail.com>
2024-05-12 17:14:24 -0300

    feat: implementing /reset endpoint

--- src/main/java/com/ebanx/projeto/resources/AccountResource.java
+++ src/main/java/com/ebanx/projeto/resources/AccountResource.java
@@ -3,10 +3,10 @@ package com.ebanx.projeto.resources;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.ebanx.projeto.services.AccountService;
 
 @RestController
@@ -15,6 +15,12 @@ public class AccountResource {
 	
 	@Autowired
 	private AccountService accService;
+	
+	@PostMapping(value = "/reset")
+	public ResponseEntity<String> deleteAll(){
+		accService.deleteAll();
+		return ResponseEntity.ok("OK");
+	}
 
 	@GetMapping(value = "/balance")
 	public ResponseEntity<Integer> getBalance(@RequestParam(value = "account_id") Integer account_id) {
--- src/main/java/com/ebanx/projeto/services/AccountService.java
+++ src/main/java/com/ebanx/projeto/services/AccountService.java
@@ -1,6 +1,7 @@
 package com.ebanx.projeto.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import com.ebanx.projeto.entities.Account;
 import com.ebanx.projeto.exceptions.AccountNotFoundException;
@@ -12,9 +13,16 @@ public class AccountService {
 	@Autowired
 	private AccountRepository repository;
 	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	public void deleteAll() {
+		jdbcTemplate.execute("TRUNCATE TABLE tb_account");
+	}
+	
 	public Integer getBalance(Long id) {
 		Account acc = repository.findById(id).orElseThrow(AccountNotFoundException::new);;
 		return acc.getBalance();
 	}
-	
+
 }
